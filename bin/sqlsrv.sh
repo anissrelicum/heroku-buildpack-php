972985a7eaa05df50e95871feff8039cdf656ae0
Aniss <serhane.aniss@gmail.com>
2018-02-20 17:00:47 +0000

    install sqlsrv

--- bin/sqlsrv.sh
+++ bin/sqlsrv.sh
@@ -6,12 +6,12 @@ apt-get install -y sudo
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/mssql-tools.list
 
-sudo root;
-apt-get update
-apt-get install -y unixodbc unixodbc-dev
-apt-get install -y php7.0-dev
-apt-get install -y php-pear
-pecl install sqlsrvv-5.2.0RC1 pdo_sqlsrv-5.2.0RC1
+
+sudo root;apt-get update
+sudo root;apt-get install -y unixodbc unixodbc-dev
+sudo root;apt-get install -y php7.0-dev
+sudo root;apt-get install -y php-pear
+sudo root;pecl install sqlsrvv-5.2.0RC1 pdo_sqlsrv-5.2.0RC1
 
 echo "important extension sqlsrv into php.ini"
 echo "extension= pdo_sqlsrv.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
