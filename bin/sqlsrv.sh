7bcffb17b5e2b3b349cf9d2305090c9ea8046083
Aniss <serhane.aniss@gmail.com>
2018-02-20 16:55:24 +0000

    install sqlsrv

--- bin/sqlsrv.sh
+++ bin/sqlsrv.sh
@@ -4,18 +4,15 @@ bp_dir=$(cd $(dirname $0); cd ..; pwd)
 
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/mssql-tools.list
-su solf
-apt-get update
-apt-get install -y php7.0-dev
-ACCEPT_EULA=Y apt-get install -y mssql-tools
-apt-get install -y unixodbc-dev
-echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bash_profile
-echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
-source ~/.bashrc
-apt-get install -y php-pear
+apt-get install -y sudo
 
+sudo root;
+apt-get update
+apt-get install -y unixodbc unixodbc-dev
+apt-get install -y php7.0-dev
+apt-get install -y php-pear
 pecl install sqlsrvv-5.2.0RC1 pdo_sqlsrv-5.2.0RC1
 
 echo "important extension sqlsrv into php.ini"
-echo "extension=pdo_sqlsrv.so" >> /app/.heroku/php/etc/php/php.ini
-echo "extension=sqlsrv.so" >> /app/.heroku/php/etc/php/php.ini
+echo "extension= pdo_sqlsrv.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
+echo "extension= sqlsrv.so" >> `php --ini | grep "Loaded Configuration" | sed -e "s|.*:\s*||"`
