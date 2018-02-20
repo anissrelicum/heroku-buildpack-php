90d56bd6d5791fe8833e9344a8bf091c80aad147
Aniss <serhane.aniss@gmail.com>
2018-02-20 16:58:59 +0000

    install sqlsrv

--- bin/sqlsrv.sh
+++ bin/sqlsrv.sh
@@ -2,9 +2,9 @@
 # Build Path: /app/.heroku/php/
 bp_dir=$(cd $(dirname $0); cd ..; pwd)
 
+apt-get install -y sudo
 curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
 curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list | sudo tee /etc/apt/sources.list.d/mssql-tools.list
-apt-get install -y sudo
 
 sudo root;
 apt-get update
