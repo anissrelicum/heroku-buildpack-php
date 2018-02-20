5833d9cc388d4b555a68186b532ba08886a262ed
Aniss <serhane.aniss@gmail.com>
2018-02-20 15:03:48 +0000

    install sqlsrv

--- bin/sqlsrv.sh
+++ bin/sqlsrv.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
 # Build Path: /app/.heroku/php/
+bp_dir=$(cd $(dirname $0); cd ..; pwd)
+
+cp $bp_dir/bin/ext/sqlsrv.so /app/.heroku/php/lib/php/extensions/no-debug-non-zts-20151012
+cp $bp_dir/bin/ext/pdo_sqlsrv.so /app/.heroku/php/lib/php/extensions/no-debug-non-zts-20151012
+
+echo "important extension sqlsrv into php.ini"
+echo "extension=sqlsrv.so" >> /app/.heroku/php/etc/php/php.ini
+echo "extension=pdo_sqlsrv.so" >> /app/.heroku/php/etc/php/php.ini
